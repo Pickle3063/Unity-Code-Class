b86b0617214e2275fe1235f84fb203edc5947bc8
Pickle3063 <35244336+Pickle3063@users.noreply.github.com>
2019-03-01 12:12:34 -0600

    added comments to script

--- MP_Disappear.cs
+++ MP_Disappear.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class MP_Disappear : MonoBehaviour
 {
 
-    //public Light myLight;
-    [SerializeField]
+    //setting a timer that can be modified in the inspector
     float timePass;
     [SerializeField]
     float timer;
 
     void Update()
     {
+        //the timer which runs in update, goes by seconds
         timePass += Time.deltaTime;
+        //if the timer is done
         if (timePass > timer)
         {
+            //toggle the mesh and collider off and on
             gameObject.GetComponent<MeshRenderer>().enabled = !gameObject.GetComponent<MeshRenderer>().enabled;
             gameObject.GetComponent<Collider>().enabled = !gameObject.GetComponent<Collider>().enabled;
+            //reset timer
             timePass = 0;
 
         }
