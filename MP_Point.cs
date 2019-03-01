ba1893acc5f2d02bb6c9a060106dbe150f878dde
Pickle3063 <35244336+Pickle3063@users.noreply.github.com>
2019-03-01 12:19:43 -0600

    added comments to the script

--- MP_Point.cs
+++ MP_Point.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MP_Point : MonoBehaviour {
+	//make a variable to call the MP_Score script easier
     MP_Score counter;
 	// Use this for initialization
 	void Start () {
+		//set the variable to the MP_Score script
         counter = FindObjectOfType<MP_Score>();
+		//call the IsAPoint function from the MP_Score script
         counter.IsAPoint();
 	}
 	
@@ -15,11 +18,14 @@ public class MP_Point : MonoBehaviour {
 		
 	}
 
+	//check if player entered the trigger collider
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+		//tell the MP_Score script you collected this point
             counter.GotPoint();
+		//get rid of this point
             Destroy(gameObject);
         }
     }
