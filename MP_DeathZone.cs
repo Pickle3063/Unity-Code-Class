c4c13d55b30a4dba7db55c739eca3960fde3b34f
Pickle3063 <35244336+Pickle3063@users.noreply.github.com>
2019-05-03 11:46:42 -0500

    comments

--- MP_DeathZone.cs
+++ MP_DeathZone.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//you need this to restart the scene
 using UnityEngine.SceneManagement;
 
 public class MP_DeathZone : MonoBehaviour {
 
+    //when colliding with a player
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            //restart scene
             int scene = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(scene, LoadSceneMode.Single);
         }
