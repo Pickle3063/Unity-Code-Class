458cc395912f2532f0ffb84b1d60199cd0f854a5
Pickle3063 <35244336+Pickle3063@users.noreply.github.com>
2019-05-03 11:57:09 -0500

    comments

--- MP_Follow.cs
+++ MP_Follow.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class MP_Follow : MonoBehaviour {
 
-    public float speed;
-    public GameObject player;
+	//so we can set the speed of the object in inspector and get a reference to the player
+    	public float speed;
+    	public GameObject player;
 
 	// Update is called once per frame
 	void Update () {
 
-        //transform.LookAt(player.transform.position);
-        transform.position = Vector3.Lerp(transform.position, player.transform.position, Time.deltaTime * speed);
+		//simply pulls the object towards the player
+        	transform.position = Vector3.Lerp(transform.position, player.transform.position, Time.deltaTime * speed);
 	}
 }
